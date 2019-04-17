c55beba59df4d484c38a0d0c8873762da79ad09d
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-04-16 20:23:49 -0400

    final

--- javascript/app.js
+++ javascript/app.js
@@ -3,8 +3,6 @@
 //countdown 30 seconds
 //if clock reaches 0
 
-// var startGame = $("#start").on('click', function() {
-//  });
 
 //Timer
 var count = 30;
@@ -12,44 +10,70 @@ var interval;
 clockRunning = false;
 
 
-function startClock(){
-    interval = setInterval(countDown, 1000)   
+function startClock() {
+    interval = setInterval(countDown, 1000)
 }
 
-function countDown(){
+function countDown() {
     count--;
-    
+
     $("#totalTime").text(count)
-    if(count === 0){
+    if (count === 0) {
         stop()
     }
-    
+
 }
-    
+
 function stop() {
     clearInterval(interval);
     clockRunning = false;
 }
 
-$(document).ready(function() {
+$(document).ready(function () { //jquery has to go in here
     $("#timer").hide();
 
-    $("#btn-start").click(function ()
-        {
-            $("#timer").show();
-            $(this).hide();
-            startClock();
+    $("#btn-start").click(function () {
+        $("#timer").show();
+        $(this).hide();
+        startClock();
+        $("#triviaGame").show();
+    });
+
+    //function to check the answers      
+
+    $("#submit").click(function() {
+        var question1 = $("input[name='question1']:checked").val();
+        var question2 = $("input[name='question2']:checked").val();
+        var question3 = $("input[name='question3']:checked").val();
+        var correct = 0;
+        var wrong = 0;
+
+        console.log(question1);
+
+        if (question1 == "Canada") {
+            correct++;
+        } else {
+            wrong++;
         }
-    );
+        if (question2 == "Peru") {
+            correct++;
+        } else {
+            wrong++;
+        }
+        if (question3 == "Damascus") {
+            correct++;
+        } else {
+            wrong++;
+        }
 
+        $('#afterSubmit').show();
+        $('#correctAnswers').text("You got " + correct + " correct answers!");
+        $("#wrongAnswers").text("You got " + wrong + " wrong answers!");
+        $("#trivia").hide();
+        $("#timer").hide();
+    })
 });
-var submitButton = $("#submit").on("click", function(){
-    document.getElementById("afterSubmit").style.visibility="visible"
-alert("You Won!")
-}); 
-            
 
-
-
+//once timer hits 0, display alert (time's up)
 
 
